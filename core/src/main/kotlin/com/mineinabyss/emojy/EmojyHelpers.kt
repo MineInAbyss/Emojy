95dce0fe12e01c2aa320c548099b3396773af1a3
Boy <sivertpaulsen2@gmail.com>
2023-11-27 19:00:15 +0100

    fix: argument parsing

--- core/src/main/kotlin/com/mineinabyss/emojy/EmojyHelpers.kt
+++ core/src/main/kotlin/com/mineinabyss/emojy/EmojyHelpers.kt
@@ -17,13 +17,14 @@ fun Component.replaceEmoteIds(player: Player? = null, insert: Boolean = true): C
     var msg = GlobalTranslator.render(this, player?.locale() ?: Locale.US)
     val serialized = msg.serialize()
 
-    emojy.emotes.filter { ":${it.id}.*:".toRegex() in serialized && it.checkPermission(player) }.forEach { emote ->
-        val colorable = ":${emote.id}\\|(c|colorable):".toRegex() in serialized
-        val bitmapIndex = ":${emote.id}\\|([0-9]+):".toRegex().find(serialized)?.groupValues?.get(1)?.toIntOrNull() ?: -1
+    emojy.emotes.filter { ":${it.id}(\\|.*?)?:".toRegex() in serialized && it.checkPermission(player) }.forEach { emote ->
+        val match = ":${emote.id}(\\|(c|colorable|\\d+))*:".toRegex().find(serialized)?.value
+        val colorable = "\\|(c|colorable)".toRegex() in serialized
+        val bitmapIndex = match?.let { "\\|([0-9]+)".toRegex().find(it)?.groupValues?.get(1)?.toIntOrNull() } ?: -1
 
         msg = msg.replaceText(
             TextReplacementConfig.builder()
-                .match(":${emote.id}.*:")
+                .match(":${emote.id}(\\|.*?)?:")
                 .replacement(emote.formattedUnicode(insert = insert, colorable = colorable, bitmapIndex = bitmapIndex))
                 .build()
         )
