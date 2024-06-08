fe06198372bdcb0d7ea8c78553e55e6bae24c290
Boy <sivertpaulsen2@gmail.com>
2024-06-08 10:00:44 +0200

    feat: handle tablist playernames

--- v1_20_R4/src/main/kotlin/com/mineinabyss/emojy/nms/v1_20_R4/EmojyNMSHandler.kt
+++ v1_20_R4/src/main/kotlin/com/mineinabyss/emojy/nms/v1_20_R4/EmojyNMSHandler.kt
@@ -114,6 +114,13 @@ class EmojyNMSHandler(emojy: EmojyPlugin) : IEmojyNMSHandler {
 
                                 packet
                             }
+                            is ClientboundPlayerInfoUpdatePacket ->
+                                ClientboundPlayerInfoUpdatePacket(packet.actions(), packet.entries().map {
+                                    ClientboundPlayerInfoUpdatePacket.Entry(
+                                        it.profileId, it.profile, it.listed, it.latency, it.gameMode,
+                                        it.displayName?.transformEmotes(connection.locale()), it.chatSession
+                                    )
+                                })
                             else -> packet
                         }, promise
                     )
